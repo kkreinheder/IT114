f385a76aba430c8952a2bdce678324259a6d92b3
kkreinheder <kk566@njit.edu>
2019-09-26 11:46:21 -0400

    Updated server to send client highest integer

--- SampleSocketServer.java
+++ SampleSocketServer.java
@@ -9,14 +9,13 @@ import java.net.Socket;
 
 public class SampleSocketServer 
 {
-	int port = 3002;
 	
 	public SampleSocketServer() 
 	{
 	}
 	public void start(int port)
 	{
-		this.port = port;
+	//	this.port = port;
 		System.out.println("Waiting for client");
 		try (ServerSocket serverSocket = new ServerSocket(port);
 				Socket client = serverSocket.accept();
@@ -24,11 +23,34 @@ public class SampleSocketServer
 				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) 
 		{
 			System.out.println("Client connected, waiting for message");
-			String fromClient = "", toClient = "";
+			String fromClient = ""; 
+			int toClient =0;
 			
 			while((fromClient = in.readLine()) != null)
 			{
 				System.out.println("Message from client: " +fromClient);
+			//	List<String> reversedInput = Arrays.asList(fromClient.split(""));
+			//	Collections.reverse(reversedInput);
+			//	toClient = String.join("", reversedInput);
+			
+				String[] nums = fromClient.split(" ");
+				int[] n1 = new int[nums.length];
+				for(int n = 0; n < nums.length; n++) 
+				{
+				   n1[n] = Integer.parseInt(nums[n]);
+				}
+				
+				int max = 0;
+				for(int i=0; i<n1.length; i++)
+				{
+					if(n1[i] > max)
+					{
+						max = n1[i];
+					}
+				} 
+				toClient = max;
+				System.out.println("Sending to client: ");
+				
 				if("kill server".equalsIgnoreCase(fromClient))
 				{
 					System.out.println("Client killed server process");
@@ -36,7 +58,7 @@ public class SampleSocketServer
 				}
 				else
 				{
-					System.out.println("From Client: " + fromClient);
+					out.println("Largest number: " +toClient);
 				}
 			}
 		}
@@ -56,11 +78,28 @@ public class SampleSocketServer
 			}
 		}
 	}
-	public static void main(String[] arg)
+	
+	public static void main(String[] args)
 	{
 		System.out.println("Starting Server");
 		SampleSocketServer server = new SampleSocketServer();
-		server.start(3002);
+		int port = -1;
+		if(args.length > 0)
+		{
+			try
+			{
+				port = Integer.parseInt(args[0]);
+			}
+			catch(Exception e)
+			{
+				System.out.println("Invalid port: " + args[0]);
+			}
+		}
+		if(port > -1)
+		{
+			System.out.println("Server listening on port: " + port);
+			server.start(port);
+		}
 		System.out.println("Server Stopped");
 	}
 }
